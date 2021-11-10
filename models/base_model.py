04c705bd5759710816cedc09201ce098487d598b
Yohannes251 <yohannes.z251@gmail.com>
2021-11-10 10:01:33 +0000

    Updated BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -5,21 +5,34 @@ This module serves as a basement for future modules to inherit from.
 
 
 from uuid import uuid4
-from datetime import datetime, date
+from datetime import datetime
 
 
 class BaseModel:
     """ This class defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes attributes"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            str_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    self.__dict__[key] = value
+            self.__dict__["created_at"] = datetime.strptime(self.created_at,
+                                                            "%Y-%m-%dT%H:"
+                                                            "%M:%S.%f")
+            self.__dict__["updated_at"] = datetime.strptime(self.updated_at,
+                                                            "%Y-%m-%dT%H:" +
+                                                            "%M:%S.%f")
 
     def __str__(self):
         """Yields readable string of the instance"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
         """Update the attribute updated_at with the current datetime"""
@@ -30,6 +43,8 @@ class BaseModel:
         Returns a dictionary containing all keys/values of the instance
         """
         self.__dict__["__class__"] = type(self).__name__
-        self.__dict__["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.__dict__["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.__dict__["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:" +
+                                                               "%M:%S.%f")
+        self.__dict__["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:" +
+                                                               "%M:%S.%f")
         return self.__dict__
