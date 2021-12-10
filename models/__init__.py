4112fc13d7f9359524ec4d8c3b13b7c6f5003edc
Yohannes251 <yohannes.z251@gmail.com>
2021-12-10 16:05:27 +0000

    Imported new classes

--- models/__init__.py
+++ models/__init__.py
@@ -7,8 +7,16 @@ Initializes package
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-classes = {"BaseModel": BaseModel, "User": User}
+classes = {"BaseModel": BaseModel, "User": User,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
 
 storage = FileStorage()
 FileStorage.reload(storage)
