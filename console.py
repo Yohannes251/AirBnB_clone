973d31b6180638a0fe9358bb31d79db4554739fa
Yohannes251 <yohannes.z251@gmail.com>
2021-12-10 13:00:21 +0000

    Imported the new classes

--- console.py
+++ console.py
@@ -10,6 +10,11 @@ import shlex
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,12 @@ This module defines a FileStorage class that performs JSON operations.
 import json
 import models
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
